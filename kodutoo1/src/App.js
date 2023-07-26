25bbcea6f303e97423cf70386181f24ddea07340
Karirinana <kari6a1998@gmail.com>
2023-07-26 17:49:13 +0300

    kodutoo1

---
+++ kodutoo1/src/App.js
@@ -0,0 +1,15 @@
+//import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <button className='nupp'>Olen rohelise tagataustaga nupp, mille sees on helehall tekst</button>
+      <p className='sinineTekst'>Olen sinine tekst, millele hiirega peale minnes muuda selle teksti suurus suuremaks</p>
+      <p className='rohelineTekst'>Olen roheline tekst</p>
+      <img src="https://play-lh.googleusercontent.com/8QnH9AhsRfhPott7REiFUXXJLRIxi8KMAP0mFAZpYgd44OTOCtScwXeb5oPe1E4eP4oF" alt="Kits" className='pilt'/>
+    </div>
+  );
+}
+
+export default App;
