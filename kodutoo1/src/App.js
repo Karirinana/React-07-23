a8bb2f8df54ac35a6cac5b35617480c0155958e3
Karirinana <kari6a1998@gmail.com>
2023-07-30 23:47:14 +0300

    Sisse/välja log

--- kodutoo1/src/App.js
+++ kodutoo1/src/App.js
@@ -3,10 +3,37 @@ import './App.css';
 import Avaleht from '../../kodutoo1/src/pages/Avaleht';
 import Kontakt from '../../kodutoo1/src/pages/Kontakt';
 import Meist from '../../kodutoo1/src/pages/Meist';
+import { useRef, useState } from 'react';
 
 function App() {
+  const [sisselogitud, muudaSisselogitud] = useState("ei");
+  const [sonum, muudaSonum] = useState ("");
+  const kasutajaNimiRef = useRef();
+  const paroolRef = useRef(); 
+
+  const logiSisse = () => {
+    if (paroolRef.current.value === "123"){
+      muudaSisselogitud("jah");
+      muudaSonum(kasutajaNimiRef.current.value + ", Oled sisselogitud");
+    } else {
+      muudaSonum("Vale parool");
+    }
+  }
+
+
   return (
     <div className="App">
+      <div>{sonum}</div>
+      { sisselogitud === "ei" && <div>
+        <label>Kasutajanimi</label> <br />
+        <input ref={kasutajaNimiRef} type="text" /> <br />
+        <label>Parool</label> <br />
+        <input ref={paroolRef} type="password" /> <br />
+      </div>}
+
+      { sisselogitud === "ei" && <button onClick={logiSisse}>Logi sisse</button>}
+      { sisselogitud === "jah" && <button onClick={() => muudaSisselogitud("ei")}>Logi välja</button>}
+    
       <Link to="/">
         <button>Avaleht</button>
       </Link>
