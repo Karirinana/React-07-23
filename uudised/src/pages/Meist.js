7042c3f454052e0791a166b6e1f71c0ff959f8e9
Karirinana <kari6a1998@gmail.com>
2023-07-30 17:28:45 +0300

    Buttons  and functions

--- uudised/src/pages/Meist.js
+++ uudised/src/pages/Meist.js
@@ -1,23 +1,30 @@
+import {useState} from "react";
+
 function Meist() {
+
+    const [kontakt, n2itaKontakt] = useState("");
+
     return ( <div>
         <div>See on meist leht, nähtav localhost:3000/meist aadressil</div>
         <div>Meie töötajad:</div>
         <br />
         <div>Ivan Ivanov</div>
-        <div>+372 111111111</div>
         <div>Uudisklippide taustamismuusika</div>
-        <br />
+        <button onClick={() => n2itaKontakt('+372 111111111')}>Võta ühendust</button>
+        <br /> <br />
         <div>Petr Petrov</div>
-        <div>+372 222222222</div>
         <div>Püstolreporter</div>
-        <br />
+        <button onClick={() => n2itaKontakt('+372 222222222')}>Võta ühendust</button>
+        <br /> <br />
         <div>Sergei Sergeev</div>
-        <div>+372 333333333</div>
         <div>Uudiste kujundamine</div>
-        <br />
+        <button onClick={() => n2itaKontakt('+372 333333333')}>Võta ühendust</button>
+        <br /> <br />
         <div>Leonid Leonidov</div>
-        <div>+372 444444444</div>
         <div>Välisturungute spetsialist</div>
+        <button onClick={() => n2itaKontakt('+372 444444444')}>Võta ühendust</button>
+        <br /> <br />
+        { kontakt !== "" && <div>Tema kontakt: {kontakt}</div>}
     </div> );
 }
 
