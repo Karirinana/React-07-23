84bdc3533e85021a0ccca502ca688f9d3b9e76a1
Karirinana <kari6a1998@gmail.com>
2023-08-23 01:29:42 +0300

    toastify set up, id unique check

--- webshop/src/pages/admin/AddProduct.jsx
+++ webshop/src/pages/admin/AddProduct.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 import { useRef } from "react";
 import { useTranslation } from 'react-i18next';
+import { ToastContainer, toast } from 'react-toastify';
 
-import productsFromfile from "../../data/products.json";
+import productsFromFile from "../../data/products.json";
 
 function AddProduct() {
   const {t} = useTranslation();
@@ -19,25 +20,25 @@ function AddProduct() {
 
   const addNewProduct = () => {
     if (idRef.current.value === "") {
-      newMessage(t("you-can-not-add-product-with-empty-id"));
+      toast(t("you-can-not-add-product-with-empty-id"));
       return;
     } 
     if (nameRef.current.value === "") {
-      newMessage("noNameInserted");
+      toast(t("noNameInsertedError"));
       return;
     } 
     if (nameRef.current.value[0].toUpperCase() !== nameRef.current.value[0]) {
-      newMessage(t("lowerCaseLetterNameError"));
+      toast(t("lowerCaseLetterNameError"));
       return;
     } 
     if (pictureRef.current.value.includes(" ")) {
-      newMessage("pictureWithEmptyNameError");
+      toast(t("pictureWithEmptyNameError"));
       return;
     } 
       newMessage(
         t("newProduct") + " " + nameRef.current.value + " " + t("NewProductAddedSuccessfully")
       );
-      productsFromfile.push({
+      productsFromFile.push({
         name: nameRef.current.value,
         id: Number(idRef.current.value),
         price: Number(priceRef.current.value),
@@ -47,15 +48,28 @@ function AddProduct() {
         active: activeRef.current.checked,
       });
     }
+    
+  const [idUnique, setIdUnique] = useState(true);
+
+  const checkIdUniqueness = () => {
+   const index = productsFromFile.findIndex(product => product.id === Number(idRef.current.value))
+
+   if (index === -1) {
+    setIdUnique(true);
+   } else {
+    setIdUnique(false);
+   }
+  }  
 
   return (
     <div>
+      {idUnique === false && <div>{t("id-is-not-unique")}</div>} <br />
       <div>{message}</div> <br />
       <label>{t("product-name")}</label> <br />
       <input ref={nameRef} type="text" />
       <br />
       <label>{t("product-id")}</label> <br />
-      <input ref={idRef} type="number" />
+      <input className={idUnique === false ? "error" : undefined} ref={idRef} onChange={checkIdUniqueness} type="number" />
       <br />
       <label>{t("product-price")}</label> <br />
       <input ref={priceRef} type="number" />
@@ -73,6 +87,11 @@ function AddProduct() {
       <input ref={activeRef} type="checkbox" />
       <br />
       <button onClick={addNewProduct}>{t("add")}</button>
+      <ToastContainer 
+      position="bottom-right"
+      autoClose={5000}
+      theme="colored"
+      />
     </div>
   );
 }
