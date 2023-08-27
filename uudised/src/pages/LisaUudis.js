13ec852d2275d6b26ebac9384cc072926c4d09f6
Karirinana <kari6a1998@gmail.com>
2023-08-27 22:02:43 +0300

    Uudiste nimetus kontroll

--- uudised/src/pages/LisaUudis.js
+++ uudised/src/pages/LisaUudis.js
@@ -1,21 +1,38 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState } from "react";
 
 function LisaUudis() {
-    const uudiseRef = useRef();
+  const uudiseRef = useRef();
+  const [sonum, uuendaSonum] = useState("");
 
-    const lisaUusUudis = () => {
-        const uudised = JSON.parse(localStorage.getItem("uudised")) || [];
-        uudised.push(uudiseRef.current.value);
-        localStorage.setItem("uudised", JSON.stringify(uudised));
+  const lisaUusUudis = () => {
+    const uudised = JSON.parse(localStorage.getItem("uudised")) || [];
+    uudised.push(uudiseRef.current.value);
+    localStorage.setItem("uudised", JSON.stringify(uudised));
+  };
+
+  const kontroll = () => {
+    uuendaSonum("");
+
+    if (
+      uudiseRef.current.value.charAt(0) ===
+      uudiseRef.current.value.charAt(0).toLowerCase()
+    ) {
+      uuendaSonum("Sisestatud uudise nimetus algab väikse tähega. Palun parandage!");
     }
+
+    if (uudiseRef.current.value.includes("  ")) {
+      uuendaSonum("Sisestatud kaks tühikut. Palun parandage!");
+    }
+  };
 
   return (
     <div>
-        <label htmlFor="">Uudise nimi:</label>
-        <input ref={uudiseRef} type="text" />
-        <button onClick={lisaUusUudis}>Lisa uudis</button>
+      <div>{sonum}</div>
+      <label htmlFor="">Uudise nimi:</label>
+      <input onChange={kontroll} ref={uudiseRef} type="text" />
+      <button onClick={lisaUusUudis}>Lisa uudis</button>
     </div>
-  )
+  );
 }
 
-export default LisaUudis
+export default LisaUudis;
