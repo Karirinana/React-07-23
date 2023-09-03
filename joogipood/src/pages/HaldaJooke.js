9eedd4440766520d12550c14042dac287a6cf8a1
Karirinana <140316728+Karirinana@users.noreply.github.com>
2023-09-03 22:34:50 +0300

    halda/lisa jook to database kodutoo

--- joogipood/src/pages/HaldaJooke.js
+++ joogipood/src/pages/HaldaJooke.js
@@ -1,26 +1,35 @@
-import React, { useState, useRef } from 'react';
-import joogidFailist from "../data/joogid.json";
+import React, { useEffect, useState, useRef } from 'react';
+import config from "../data/config.json";
 
 function HaldaJooke() {
-  const [joogid, uuendaJoogid] = useState(joogidFailist);
+  const [joogid, uuendaJoogid] = useState([]);
+
+  useEffect(() => {
+    fetch(config.joogidDbUrl)
+      .then(res => res.json())
+      .then(json => uuendaJoogid(json || []));
+  }, []);
 
   const joogidRef = useRef();
 
   const kustuta = (index) => {
-    joogidFailist.splice(index,1);
-    uuendaJoogid(joogidFailist.slice());
+    joogid.splice(index,1);
+    uuendaJoogid(joogid.slice());
+    fetch(config.joogidDbUrl, {"method": "PUT", "body": JSON.stringify(joogid)})
   }
 
   const lisa = () => {
-    joogidFailist.push(joogidRef.current.value);
-    uuendaJoogid(joogidFailist.slice());
+    joogid.push({"nimi": joogidRef.current.value});
+    uuendaJoogid(joogid.slice());
+    fetch(config.joogidDbUrl, {"method": "PUT", "body": JSON.stringify(joogid)})
   }
 
   return (
     <div>
       {joogid.map((jook, index) => 
         <div key={jook}>
-          {jook} <button onClick={() => kustuta (index)}>X</button>
+          {jook.nimi} 
+          <button onClick={() => kustuta (index)}>X</button>
         </div>)}
       
       <label htmlFor="">Lisa oma joogi: </label>
--- joogipood/src/pages/LisaJook.js
+++ joogipood/src/pages/LisaJook.js
@@ -1,19 +1,29 @@
-import React from 'react';
-import joogidFailist from "../data/joogid.json";
+import React, { useEffect, useState } from 'react';
 import { useRef } from 'react';
+import config from "../data/config.json";
 
 function LisaJook() {
   const joogidRef = useRef();
 
+  const [joogid, setJoogid] = useState([]);
+
+  useEffect(() => {
+    fetch(config.joogidDbUrl)
+      .then(res => res.json())
+      .then(json => setJoogid(json || []));
+  }, []);
+
   const lisa = () => {
-    joogidFailist.push(joogidRef.current.value);
+    joogid.push({"nimi": joogidRef.current.value});
+    setJoogid(joogid.slice());
+    fetch(config.joogidDbUrl, {method: "PUT", body: JSON.stringify(joogid)})
   }
 
   return (
     <div>
       <label htmlFor="">Lisa oma joogi: </label>
       <input ref={joogidRef} type="text" />
-      <button onClick={lisa}>lisa</button>
+      <button onClick={() => lisa ()}>lisa</button>
     </div>
   )
 }
