34abad585711192f6fdea75833fca17a7002447a
Karirinana <kari6a1998@gmail.com>
2023-09-05 23:54:16 +0300

    Bakery shop delete employee func + form validation

--- bakery-shop/src/pages/Employees.jsx
+++ bakery-shop/src/pages/Employees.jsx
@@ -1,16 +1,72 @@
+import { useEffect, useRef, useState } from "react";
 import { Button, Table } from "react-bootstrap";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function Employees() {
 
-  // TODO: Load data from backend service
+  const [employees, setEmployees] = useState([]);
+
+  const idRef = useRef();
+  const nameRef = useRef();
+  const lastNameRef = useRef();
+  const emailRef = useRef();
+  const avatarRef = useRef();
+
+  useEffect(() => {
+    fetch("https://reqres.in/api/users")
+    .then(res => res.json())
+    .then(json => setEmployees(json.data))
+  }, []);
 
   const addEmployee = () => {
-    // TODO: Add validations
-    // TODO: Add an employee to the table
+    if (idRef.current.value === "") {
+      toast("Please add employee ID");
+      return;
+    }
+
+    if (Number.isNaN(Number(idRef.current.value))) {
+      toast("Please check employee ID. It should contain only numbers");
+      return;
+    }
+
+    if (nameRef.current.value === "" || lastNameRef.current.value === "") {
+      toast("Please check employee name");
+      return;
+    }
+
+    const letterValidator = /^[A-Za-z\s']+$/;
+    if (letterValidator.test(nameRef.current.value) === false || letterValidator.test(lastNameRef.current.value) === false) {
+      toast("Employee name can contain only letters");
+      return;
+    }
+
+    const emailValidator = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    if (emailValidator.test(emailRef.current.value) === false) {
+      toast("Please check employee email address");
+    }
+
+    if (emailRef.current.value === "") {
+      toast("Please add employee email");
+      return;
+    }
+
+    if (avatarRef.current.value === "") {
+      toast("Please add employee photo");
+      return;
+    }
+    employees.push({
+      id: Number(idRef.current.value),
+      email: emailRef.current.value,
+      first_name: nameRef.current.value,
+      last_name: lastNameRef.current.value,
+      avatar: avatarRef.current.value
+    })
   }
 
   const deleteEmployee = (index) => {
-    // TODO: Delete an employee from the table
+    employees.splice(index,1);
+    setEmployees(employees.slice());
   }
 
   return (<div>
@@ -22,34 +78,34 @@ function Employees() {
           <th scope="col">ID</th>
           <th scope="col">Name</th>
           <th scope="col">Email</th>
-          {/* <!-- TODO: Add a column for an avatar --> */}
+          <th scope="col">Avatar</th>
           <th scope="col">Actions</th>
         </tr>
         </thead>
         <tbody>
-        <tr>
-          <td>123</td>
-          <td>Added name 1</td>
-          <td>email@email.com</td>
-          <td><Button type="button" variant="danger">Delete</Button></td>
-        </tr>
-        <tr>
-          <td>124</td>
-          <td>Added name 2</td>
-          <td>email2@email.com</td>
-          <td><Button type="button" variant="danger">Delete</Button></td>
-        </tr>
-
+        {employees.map((employee, index) => (
+          <tr key={employee.id}>
+            <td>{employee.id}</td>
+            <td>{employee.first_name + " " + employee.last_name}</td>
+            <td>{employee.email}</td>
+            <td><img src={employee.avatar} alt="" /></td>
+            <td>
+              <button onClick={() => deleteEmployee(index)}>x</button>
+            </td>
+          </tr>
+        ))}
         <tr className="input-row">
-          <td><input type="text" placeholder="ID" className="form-control"/></td>
-          <td><input type="text" placeholder="Name" className="form-control"/></td>
-          <td><input type="text" placeholder="Email" className="form-control"/></td>
-          <td><Button type="submit" variant="success">Add</Button></td>
+          <td><input ref={idRef} type="text" placeholder="ID" className="form-control"/></td>
+          <td><input ref={nameRef} type="text" placeholder="Name" className="form-control"/>
+          <input ref={lastNameRef} type="text" placeholder="Last name" className="form-control"/></td>
+          <td><input ref={emailRef} type="text" placeholder="Email" className="form-control"/></td>
+          <td><input ref={avatarRef} type="text" placeholder="Avatar" className="form-control"/></td>
+          <td><Button type="submit" variant="success" onClick={() => addEmployee(setEmployees)}>Add</Button></td>
         </tr>
         </tbody>
       </Table>
     </div>
-
+    <ToastContainer />
   </div>)
 }
 
